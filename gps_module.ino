358747eedb44b3275aa5ea294544bb3e32f5126b
Nicola Andrei-George <mailnormal1000@gmail.com>
2024-02-12 15:22:33 +0200

    Add files via upload

--- gps_module.ino
+++ gps_module.ino
@@ -1,5 +1,6 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
 
 const int RXPin = 3, TXPin = 4;
 const uint32_t baud = 9600;
@@ -9,34 +10,34 @@ TinyGPSPlus gps;
 SoftwareSerial gpsSerial(RXPin, TXPin);
 unsigned long start = 0;
 
-/*static void smartDeplay(unsigned long ms){
-  unsigned long start = millis();
-  do{
-    while(gpsSerial.available())
-      gpsSerial.encode(gpsSerial.read());
-  }while(start - millis() < ms);
-}*/
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 
 void setup() {
   Serial.begin(baud);
   gpsSerial.begin(baud);
   Serial.println("GPS MODULE:");
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-
-  Serial.println("numar sateliti: " + String(gps.satellites.value()));
-  Serial.println("latitudine: " + String(gps.location.lat()));
-  Serial.println("longitudine: " + String(gps.location.lng()));
-  Serial.println("altitudine: " + String(gps.altitude.feet()));
-
-  delay(500);
-  
+    afisare();
 }
 
 void afisare(){
-    Serial.println("numar de sateliti:" + String(gps.satellites.value()));
-    Serial.print("  ora: " + String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()));
-    Serial.print("  latitudine:" + String(gps.location.lat()));
-    Serial.print("  longitudine: " + String(gps.location.lng()));
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("sateliti: " + String(gps.satellites.value()));
+  lcd.setCursor(0, 1);
+  lcd.print("ora: " + String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()));
+
+  delay(700);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("lat: " + String(gps.location.lat()));
+  lcd.setCursor(0, 1);
+  lcd.print("long: " + String(gps.location.lng()));
+  delay(1500);
 }
